1cd39ca39f5ca73afa1480c9537fce9ef1e82e49
godwin opara <godwinopara62@yahoo.com>
2022-05-31 10:01:02 +0100

    created a file model.py and added the database models(table) for app

--- models.py
+++ models.py
@@ -1,34 +1,52 @@
+import datetime
+from tkinter import CASCADE
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Table
+from sqlalchemy.dialects.postgresql import ARRAY
 
 db = SQLAlchemy()
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(ARRAY(db.String()), nullable=False, default=[])
+    website = db.Column(db.String)
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500), nullable=False)
+    show = db.relationship('Show', backref='venue', lazy='joined', cascade='all, delete')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(ARRAY(db.String()), nullable=False, default=[])
+    website = db.Column(db.String)
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500), nullable=False)
+    show = db.relationship('Show', backref='artist', lazy='joined', cascade='all, delete')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+show = Table('show',
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False),
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False),
+    start_time = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False),
+),
